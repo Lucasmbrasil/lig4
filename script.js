c6859f9589511ce1896f77e137eb5fbc9e58cbfc
Lucas Brasil <lucasmbrasil2101@gmail.com>
2021-06-07 13:05:37 -0300

    Iniciando repositório

---
+++ script.js
@@ -0,0 +1,30 @@
+// Criar o tabuleiro 7x6 com array 2d
+const tabuleiro = 
+    // Kaio
+// Criar o tabuleiro html
+// for (let i = 0; i < map.length; i++) {
+//     let coluna = document.createElement('div')
+//     for (let j = 0; j < map[i].length; j++) {
+//         let celula = document.createElement('div')
+        // celula.setAttribute('data-coord', `${i}${j}`)
+
+    // Kaio
+// Criar 1 variável pra cada jogador
+let jogador1 =
+let jogador2 =
+// Criar 1 variável pra guardar o jogador da vez
+let jogadorAtual =
+// Criar a função pra posicionar o disco 
+function posicionarDisco(){
+// receber o jogadorAtual e no final da função fazer a mudança do jogadorAtual e já chama a função de verificação
+// Criar um handler de clique para cada coluna 
+    // Osmar
+}
+
+// Criar a função de verificação de vitória
+function verificar(){}
+    // Eduardo
+// Criar a função de verificação de empate
+function verificarEmpate(){}
+    // Lucas
+// Criar uma função pra verificar a quantidade de elementos na coluna, quando todas estiverem completas, deu empate
